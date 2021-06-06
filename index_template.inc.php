951131770edf01aeadbccff4357557393b9519d9
Drajat Hasan <drajathasan20@gmail.com>
2021-06-06 10:53:35 +0700

    First : release

---
+++ index_template.inc.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @author Drajat Hasan
+ * @email drajathasan20@gmail.com
+ * @create date 2021-06-06 08:04:24
+ * @modify date 2021-06-06 08:04:24
+ * @desc [description]
+ */
+
+// require helper
+require_once __DIR__ . '/tools/helper.php';
+
+// check direct
+isDirect();
+
+?>
+<!DOCTYPE html>
+<html lang="id">
+    <head>
+        <?php tarsiusLoad(__DIR__ . '/components/meta.php'); ?>   
+    </head>
+    
+    <body class="bg-gray-100">
+        <?php
+        // navbar
+        tarsiusLoad(__DIR__ . '/components/navbar.php');
+        ?>
+
+        <div>
+            <?php
+            // set content
+            if (!isset($_GET['p']) && !isset($_GET['search'])) {
+                // load first content
+                tarsiusLoad(__DIR__ . '/components/contents.php');
+            } 
+            else
+            {
+                // load dynamic content
+
+            }
+            ?>
+        </div>
+
+        <?php
+        // footer
+        tarsiusLoad(__DIR__ . '/components/footer.php');
+
+        // JS
+        tarsiusLoad(__DIR__.'/components/js.php');
+        ?>
+    </body>
+</html>
